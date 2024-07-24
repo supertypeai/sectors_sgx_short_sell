a61f45dc07646e5bfde6dd122719243b2a9407f1
Gulilil <juan.csantoso@gmail.com>
2024-07-24 14:29:15 +0700

    feat: finishing integrating code with the fuzz

--- function_thefuzz.py
+++ function_thefuzz.py
@@ -110,7 +110,7 @@ def preprocess_names(unique_value_short_sell: list) -> list:
   return cleaned_unique_value_short_sell
 
 
-def match_names(cleaned_unique_value_short_sell : list) -> list:
+def match_names(cleaned_unique_value_short_sell : list, unique_value_short_sell: list, companies_dict_list: dict) -> list:
   list_of_dictionaries = list()
   for i in range (len(cleaned_unique_value_short_sell)):
     original_name = unique_value_short_sell[i]
@@ -130,7 +130,7 @@ def match_names(cleaned_unique_value_short_sell : list) -> list:
     max_token_set_rat = 0
     max_partial_token_sort_ratio = 0
 
-    for company_data in companies_dict:
+    for company_data in companies_dict_list:
       c_name = company_data['name']
       c_symbol = company_data['symbol']
 
@@ -287,65 +287,65 @@ def insert_names_to_df(final_data: dict, df_short_sell: pd.DataFrame) -> pd.Data
   return df_short_sell_filled
 
 
-if __name__ == "__main__":
-  url_supabase = os.getenv("SUPABASE_URL")
-  key = os.getenv("SUPABASE_KEY")
-  supabase = create_client(url_supabase, key)
+# Only run to test the code
+# if __name__ == "__main__":
+#   url_supabase = os.getenv("SUPABASE_URL")
+#   key = os.getenv("SUPABASE_KEY")
+#   supabase = create_client(url_supabase, key)
 
-  # Get the table
-  limit = 3000
-  offset = 0
-  all_data = []
+#   # Get the table
+#   limit = 3000
+#   offset = 0
+#   all_data = []
 
-  while True:
-    response = supabase.table("sgx_short_sell").select("").range(offset, offset + limit - 1).execute()
-    data = response.data
-    all_data.extend(data)
+#   while True:
+#     response = supabase.table("sgx_short_sell").select("").range(offset, offset + limit - 1).execute()
+#     data = response.data
+#     all_data.extend(data)
 
-    if len(data) < limit:
-        break
+#     if len(data) < limit:
+#         break
 
-    offset += limit
-    print(f"Getting Data... Offset {offset} - {offset+limit-1}")
+#     offset += limit
+#     print(f"Getting Data... Offset {offset} - {offset+limit-1}")
 
-  df_short_sell = pd.DataFrame(all_data)
-  df_short_sell = df_short_sell.sort_values(['name'], ascending=True)
-  df_short_sell = df_short_sell.replace("", None)
+#   df_short_sell = pd.DataFrame(all_data)
+#   df_short_sell = df_short_sell.sort_values(['name'], ascending=True)
+#   df_short_sell = df_short_sell.replace("", None)
 
 
-  # Get the table
-  db_data = supabase.table("sgx_companies").select("").execute()
-  df_companies = pd.DataFrame(db_data.data)
+#   # Get the table
+#   db_data = supabase.table("sgx_companies").select("").execute()
+#   df_companies = pd.DataFrame(db_data.data)
 
-  # Get only the name and the symbol
-  cols = df_companies.columns.tolist()
-  cols.remove("name")
-  cols.remove("symbol")
-  df_companies_clean = df_companies.drop(cols, axis=1)
-  df_companies_clean.head()
+#   # Get only the name and the symbol
+#   cols = df_companies.columns.tolist()
+#   cols.remove("name")
+#   cols.remove("symbol")
+#   df_companies_clean = df_companies.drop(cols, axis=1)
 
-  companies_dict = df_companies_clean.to_dict(orient="records")
+#   companies_dict_list = df_companies_clean.to_dict(orient="records")
   
-  unique_value_short_sell = df_short_sell['name'].unique()
-  cleaned_unique_value_short_sell = preprocess_names(unique_value_short_sell)
-  list_of_dictionaries = match_names(cleaned_unique_value_short_sell)
-  final_data, still_null_data = vote_names(list_of_dictionaries)
+#   unique_value_short_sell = df_short_sell['name'].unique()
+#   cleaned_unique_value_short_sell = preprocess_names(unique_value_short_sell)
+#   list_of_dictionaries = match_names(cleaned_unique_value_short_sell, unique_value_short_sell, companies_dict_list)
+#   final_data, still_null_data = vote_names(list_of_dictionaries)
 
-  # save_names(final_data, still_null_data) # Only if needed
-  df_short_sell_filled = insert_names_to_df(final_data, df_short_sell)
+#   # save_names(final_data, still_null_data) # Only if needed
+#   df_short_sell_filled = insert_names_to_df(final_data, df_short_sell)
 
-  # Convert to json. Remove the index in dataframe
-  records = df_short_sell_filled.to_dict(orient="records")
+#   # Convert to json. Remove the index in dataframe
+#   records = df_short_sell_filled.to_dict(orient="records")
 
-  # Upsert to db
-  try:
-    supabase.table("sgx_short_sell").upsert(
-          records
-      ).execute()
-    print(
-        f"Successfully upserted {len(records)} data to database"
-    )
-  except Exception as e:
-    raise Exception(f"Error upserting to database: {e}")
+#   # Upsert to db
+#   try:
+#     supabase.table("sgx_short_sell").upsert(
+#           records
+#       ).execute()
+#     print(
+#         f"Successfully upserted {len(records)} data to database"
+#     )
+#   except Exception as e:
+#     raise Exception(f"Error upserting to database: {e}")
 
 
